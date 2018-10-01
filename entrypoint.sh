24da44e6602401696b959b75611ac2a62f713471
Andrew Ruder <andy@aeruder.net>
2018-10-01 07:22:14 -0500

    Got this going on raspberry pi

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -eu -o pipefail
 USER_ID=${LOCAL_USER_ID:-999}
 echo "Starting with UID : $USER_ID"
 
-useradd --shell /bin/false -u $USER_ID -o -c "" -m znc || true
+useradd --shell /bin/false -u $USER_ID -o -c "" -m znc
+chown znc /home/znc
 chown znc /home/znc/.znc
 exec /usr/sbin/gosu znc "$@"
