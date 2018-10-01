e4d0c157525ea2a43beabda87604418df5068031
Andrew Ruder <andy@aeruder.net>
2018-09-30 21:07:22 -0700

    make adduser repeatable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -eu -o pipefail
 USER_ID=${LOCAL_USER_ID:-999}
 echo "Starting with UID : $USER_ID"
 
-useradd --shell /bin/false -u $USER_ID -o -c "" -m znc
+useradd --shell /bin/false -u $USER_ID -o -c "" -m znc || true
 chown znc /home/znc/.znc
 exec /usr/sbin/gosu znc "$@"
